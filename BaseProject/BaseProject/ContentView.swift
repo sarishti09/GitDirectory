592393fd6b67c3db5eb0d1941dd878b5868b01bc
sarishti batta <sarishti.batta@netsolutions.com>
2019-11-26 14:41:00 +0530

    [Sprint 1] Initial Project

---
+++ BaseProject/BaseProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaseProject
+//
+//  Created by sarishti on 26/11/19.
+//  Copyright © 2019 sarishti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World Value")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
